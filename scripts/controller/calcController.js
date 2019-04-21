6d54fb467ecdb3c80abf6b1d78a5986c9f99b9d7
Patrick Chagas <patrickchagas21@gmail.com>
2019-04-20 22:44:47 -0300

    Ctrl + c e Ctrl + v

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -21,6 +21,36 @@ class CalcController{
 
     }
 
+    pasteFromClipboard()
+    {
+        
+        document.addEventListener('paste', e =>{
+
+            let text = e.clipboardData.getData('Text');
+
+            this.displayCalc = parseFloat(text);
+        });
+
+    }
+
+    copyToClipboard()
+    {
+
+        let input = document.createElement("input");
+
+        input.value = this.displayCalc;
+
+        document.body.appendChild(input);
+
+        input.select();
+
+        document.execCommand("Copy");
+
+        input.remove();
+
+    }
+
+
     initialize()
     {
 
@@ -34,6 +64,7 @@ class CalcController{
 
         //Atualizar Display
         this.setLastNumberToDisplay();
+        this.pasteFromClipboard();
 
     }
 
@@ -82,6 +113,11 @@ class CalcController{
                 case '9':
                     this.addOperation(parseInt(e.key));
                 break;
+
+                case 'c':
+                    if(e.ctrlKey) this.copyToClipboard();
+                break;
+
 
             }
 
